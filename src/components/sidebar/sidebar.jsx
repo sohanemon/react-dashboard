7b3b7886fa23ec0bd02443c37df625344e700da5
sohanemon <m.sohanemon@gmail.com>
2022-09-22 12:27:36 +0600

    sidebar updated

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -2,52 +2,47 @@ import { MdBorderColor, MdInventory2, MdDashboard } from "react-icons/md";
 import { BiConfused } from "react-icons/bi";
 import { RiCustomerService2Fill, RiAccountPinCircleFill } from "react-icons/ri";
 import { AiFillProject } from "react-icons/ai";
-import { useContext, useEffect, useRef } from "react";
-import { SidebarContext } from "../../App";
+import { createContext, useContext, useState } from "react";
+const PathContext = createContext();
 const Sidebar = (props) => {
-  const sidebar = useRef();
-  const { sidebarVisibility, setSidebarVisibility } =
-    useContext(SidebarContext);
-  useEffect(() => {
-    // if (sidebarVisibility) sidebar.current.classList.remove("hidden");
-    // else sidebar.current.classList.add("hidden");
-    return () => {};
-  }, [sidebarVisibility]);
+  const [pathname, setPathname] = useState("Dashboard");
 
   return (
-    <nav className='bg-pink-500 h-full pl-8' ref={sidebar}>
-      <header className='text-4xl text-white pt-4 pb-8 flex items-center gap-4'>
-        <BiConfused />
-        Accusoft
-      </header>
-      <section>
-        <div className='flex items-center text-xl gap-3 cursor-pointer font-semibold p-3 bg-white rounded-l-full text-pink-500 my-2'>
-          <MdDashboard />
-          <p>Dashbaord</p>
-        </div>
-        <div className='flex items-center text-xl gap-3 cursor-pointer font-semibold p-3  rounded-l-full text-white my-2'>
-          <RiCustomerService2Fill />
-          <p>Customer</p>
-        </div>
-        <div className='flex items-center text-xl gap-3 cursor-pointer font-semibold p-3  rounded-l-full text-white my-2'>
-          <AiFillProject />
-          <p>Projects</p>
-        </div>
-        <div className='flex items-center text-xl gap-3 cursor-pointer font-semibold p-3  rounded-l-full text-white my-2'>
-          <MdBorderColor />
-          <p>Orders</p>
-        </div>
-        <div className='flex items-center text-xl gap-3 cursor-pointer font-semibold p-3  rounded-l-full text-white my-2'>
-          <MdInventory2 />
-          <p>Inventory</p>
-        </div>
-        <div className='flex items-center text-xl gap-3 cursor-pointer font-semibold p-3  rounded-l-full text-white my-2'>
-          <RiAccountPinCircleFill />
-          <p>Accounts</p>
-        </div>
-      </section>
-    </nav>
+    <PathContext.Provider value={{ pathname, setPathname }}>
+      <nav className='bg-pink-500 h-full pl-8'>
+        <header className='text-4xl text-white pt-4 pb-8 flex items-center gap-4'>
+          <BiConfused />
+          Accusoft
+        </header>
+        <section>
+          <NavItem icon={<MdDashboard />} name='Dashboard' />
+          <NavItem icon={<RiCustomerService2Fill />} name='Customer' />
+          <NavItem icon={<AiFillProject />} name='Project' />
+          <NavItem icon={<MdBorderColor />} name='Orders' />
+          <NavItem icon={<MdInventory2 />} name='Inventories' />
+          <NavItem icon={<RiAccountPinCircleFill />} name='Account' />
+        </section>
+      </nav>
+    </PathContext.Provider>
   );
 };
 
 export default Sidebar;
+
+function NavItem({ icon, name }) {
+  const { pathname, setPathname } = useContext(PathContext);
+  return (
+    <div
+      onClick={() => {
+        setPathname(name);
+      }}
+      href={name}
+      className={`${
+        pathname === name ? "text-pink-500 bg-white" : "text-white"
+      } flex items-center  text-xl gap-3 cursor-pointer font-semibold p-3  rounded-l-full  my-2`}
+    >
+      {icon}
+      <p>{name}</p>
+    </div>
+  );
+}
